43cbf5159cde45b9caf4d2b30d564813d6917813
Aeon <renniel.pena@gmail.com>
2024-04-16 13:31:21 +0100

    Add function to calculate age using birthyear

--- counter.js
+++ counter.js
@@ -4,10 +4,15 @@ function count() {
   let numberCount = document.querySelector("h1");
   numberCount.innerHTML = counter;
 
-  if (counter % 10 === 0) {
-    alert(`Count is now ${counter}`);
-  }
+  // if (counter % 10 === 0) {
+  //   alert(`Count is now ${counter}`);
+  // }
 }
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("button").onclick = count;
+
+  setInterval(count, 1000);
+  if (count === 20) {
+    alert("Time's up!!!");
+  }
 });
